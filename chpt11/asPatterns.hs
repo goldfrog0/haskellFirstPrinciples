710130f9df3c49a78a45d48ed0cb116bcf4561e5
goldfrog0 <xraygarr@gmail.com>
2024-05-11 18:56:29 -0500

    added more aspattern practice

--- chpt11/asPatterns.hs
+++ chpt11/asPatterns.hs
@@ -1,4 +1,5 @@
 module AsPatterns where
+import Data.Char
 
 isSubseqOf :: (Eq a) => [a] -> [a] -> Bool
 isSubseqOf [] _  = True
@@ -7,3 +8,20 @@ isSubseqOf check@(c:cs) parent@(p:ps)
   | c == p = isSubseqOf cs ps
   | elem c parent = isSubseqOf check ps
   | otherwise = False 
+
+capitalizeWords :: String -> [(String, String)]
+capitalizeWords = map (\x -> (x, capWord x)) . words
+
+capWord :: String -> String
+capWord [] = []
+capWord (x:xs) = toUpper x:xs
+
+capitalizeParagraph :: String -> String
+capitalizeParagraph paragraph = unwords $ aux (words paragraph)
+  where
+    aux :: [String] -> [String]
+    aux [] = []
+    aux l@(_:[]) = l
+    aux (a:b:rest)
+      | last a == '.' = a : capWord b : aux rest
+      | otherwise = a : aux (b:rest)
